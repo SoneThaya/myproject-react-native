ff3a4ba0c6fc946e7dde3b2ce1c1dfddf3fd3282
Sone <sthaya@gmail.com>
2020-10-21 19:21:14 -0700

    worked on filter on click

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "shawn", key: "1" },
-    { name: "yoshi", key: "2" },
-    { name: "mario", key: "3" },
-    { name: "luigi", key: "4" },
-    { name: "peach", key: "5" },
-    { name: "toads", key: "6" },
-    { name: "bowser", key: "7" },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
   ]);
-  // flatlist automatically looks for a key
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
         numColumns={2}
+        keyExtractor={(item) => item.id}
         data={people}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
-
-      {/*<ScrollView>
-        {people.map((item) => (
-          <View key={item.key}>
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
             <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-        </ScrollView>*/}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -34,18 +44,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     paddingTop: 40,
     paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
+    backgroundColor: "#fff",
   },
   item: {
+    flex: 1,
+    marginHorizontal: 10,
     marginTop: 24,
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
   },
 });
